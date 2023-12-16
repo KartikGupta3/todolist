2c14a3a9fe2dc3469ea9faefcb97425750e6dc36
Kartik Gupta <kkartikgupta30@gmail.com>
2023-12-16 10:35:03 +0530

    functionality added

--- src/todo.js
+++ src/todo.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const TodoList = () => {
+  const [list, setList] = useState([]);
+  const [task, setTask] = useState("");
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    setList([...list, { id: Math.random(), task }]);
+    setTask("");
+  };
+  const handleDelete = (id) => {
+    const ulist = [...list];
+    ulist.splice(id, 1);
+    setList(ulist);
+  };
   return (
-    <div className="bg-[#2C2C2C] w-screen h-screen">
+    <div className="bg-[#2C2C2C] w-screen h-screen overflow-auto">
       <div className="flex flex-col gap-32 p-8">
         <div className="flex flex-col gap-4 items-center justify-center">
           <div class="flex flex-row gap-6 items-center">
@@ -49,64 +61,79 @@ export const TodoList = () => {
         </div>
         <div className="flex flex-col gap-9 items-center justify-center">
           <div className="flex flex-row items-center pl-20">
-            <input className="bg-white w-[720px] h-[40px] rounded-full text-left p-4"></input>
-            <button className="bg-[#4F4F4F] w-[90px] h-[40px] rounded-full text-white -translate-x-16">
+            <input
+              className="bg-white w-[720px] h-[40px] rounded-full text-left p-4"
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
+            ></input>
+            <button
+              className="bg-[#4F4F4F] w-[90px] h-[40px] rounded-full text-white -translate-x-16"
+              onClick={handleAddTask}
+            >
               <div className="font-serif">Add Task</div>
             </button>
           </div>
           <div class="border-solid border-[#ffffff] border-t border-r-[0] border-b-[0] border-l-[0] w-[850px] h-0 ml-8"></div>
-          <div className="flex flex-col gap-9 p-24">
-            <div className="flex flex-row items-center pl-8">
-              <div className="bg-[#222222] w-[720px] h-[38px] rounded-lg text-white p-1.5">
-                adad
-              </div>
-              <button className="bg-gray-300 rounded-lg -translate-x-4">
-                <svg
-                  class=""
-                  width="30"
-                  height="30"
-                  viewBox="0 0 30 30"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M24.7969 6.98438H5.20312"
-                    stroke="#222222"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M12.3282 12.3281V19.4531"
-                    stroke="#222222"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M17.6719 12.3281V19.4531"
-                    stroke="#222222"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M23.0156 6.98438V23.9062C23.0156 24.1425 22.9218 24.369 22.7547 24.536C22.5877 24.703 22.3612 24.7969 22.125 24.7969H7.87497C7.63876 24.7969 7.41223 24.703 7.2452 24.536C7.07818 24.369 6.98434 24.1425 6.98434 23.9062V6.98438"
-                    stroke="#222222"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M19.4531 6.98438V5.20313C19.4531 4.73071 19.2655 4.27764 18.9314 3.94359C18.5974 3.60954 18.1443 3.42188 17.6719 3.42188H12.3281C11.8557 3.42188 11.4026 3.60954 11.0686 3.94359C10.7345 4.27764 10.5469 4.73071 10.5469 5.20313V6.98438"
-                    stroke="#222222"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              </button>
-            </div>
+          <div className="p-24">
+            {list.length > 0 &&
+              list.map((li) => (
+                <div className="flex flex-col p-2" key={li.id}>
+                  <div className="flex flex-row items-center pl-8">
+                    <div className="bg-[#222222] w-[720px] h-[38px] rounded-lg text-white p-1.5">
+                      {li.task}
+                    </div>
+                    <button
+                      className="bg-gray-300 rounded-lg -translate-x-4"
+                      onClick={() => handleDelete(li.id)}
+                    >
+                      <svg
+                        class=""
+                        width="30"
+                        height="30"
+                        viewBox="0 0 30 30"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M24.7969 6.98438H5.20312"
+                          stroke="#222222"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                        <path
+                          d="M12.3282 12.3281V19.4531"
+                          stroke="#222222"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                        <path
+                          d="M17.6719 12.3281V19.4531"
+                          stroke="#222222"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                        <path
+                          d="M23.0156 6.98438V23.9062C23.0156 24.1425 22.9218 24.369 22.7547 24.536C22.5877 24.703 22.3612 24.7969 22.125 24.7969H7.87497C7.63876 24.7969 7.41223 24.703 7.2452 24.536C7.07818 24.369 6.98434 24.1425 6.98434 23.9062V6.98438"
+                          stroke="#222222"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                        <path
+                          d="M19.4531 6.98438V5.20313C19.4531 4.73071 19.2655 4.27764 18.9314 3.94359C18.5974 3.60954 18.1443 3.42188 17.6719 3.42188H12.3281C11.8557 3.42188 11.4026 3.60954 11.0686 3.94359C10.7345 4.27764 10.5469 4.73071 10.5469 5.20313V6.98438"
+                          stroke="#222222"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+              ))}
           </div>
         </div>
       </div>
